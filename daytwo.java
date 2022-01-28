e0a2802d62c83d807699f5c8e20712d9a3579c2b
deepgohil <84922741+deepgohil@users.noreply.github.com>
2022-01-28 23:53:34 +0530

    initial commit

---
+++ daytwo.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class daythree {
+    public static void main(String[] args) {
+        // prime vado code in root time
+    //   Scanner sc=new Scanner(System.in);
+    //   int t=sc.nextInt();
+    //   for (int i = 0; i < t; i++) {
+    //       Boolean isprime=true;
+    //       int num=sc.nextInt();
+    //       for (int j = 2; j*j <=num ; j++) {
+    //           if(num%j==0){
+    //               isprime=false;
+    //           }
+    //       }
+    //       if(isprime==true)
+    //       {
+    //           System.out.println("prime");
+    //       }
+    //       else{
+    //           System.out.println("Not prime");
+    //       }
+    //   }
+
+//////////////////////////////////////////fibbo series
+// Scanner sc=new Scanner(System.in);
+// int a=0,b=1,c;
+// int n=sc.nextInt();
+// for (int i = 0; i < n; i++) {
+//     System.out.println(a);
+//     c=a+b;
+//     a=b;
+//     b=c;
+// }
+
+///////////////////count the integer
+// Scanner sc=new Scanner(System.in);
+// int n=sc.nextInt(),i=0;
+// while(n>0)
+// {
+// n=n/10;
+// i++;
+// }
+// System.out.println(i);
+////////////////////reverse number
+// Scanner sc=new Scanner(System.in);
+// int num=sc.nextInt();
+// int rem=0;
+// while(num>10)
+// {
+//     rem=num%10;
+//     num=num/10;
+//     System.out.println(rem);
+// }
+
+  }  
+}

27e1eeda3da47cf7276a95f6b2901c8eeff61715
deepgohil <84922741+deepgohil@users.noreply.github.com>
2022-09-29 10:32:48 +0000

    balanced bracket

--- Stack.java
+++ Stack.java
@@ -41,3 +41,72 @@ for(int i=0;i<exp.length();i++)
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////balanced bracket
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+Scanner sc=new Scanner(System.in);
+String exp=sc.nextLine();
+
+System.out.println(isBalanced(exp));
+
+    }
+    public static boolean isBalanced(String exp)
+    {
+        
+Stack<Character> st=new Stack<>();
+
+
+for(int i=0;i<exp.length();i++)
+{
+    char ch=exp.charAt(i);
+    if(ch=='(' || ch=='{' || ch=='[')
+    {
+        st.push(ch);
+    }
+   else if(ch==')' || ch=='}' || ch==']')
+   {
+       if(st.size()==0)
+       {
+           return false;
+           
+       }
+       if(ch==')' && st.peek()!='(')
+       {
+           return false;
+       }
+       if(ch=='}' && st.peek()!='{')
+       {
+           return false;
+       }
+       if(ch==']' && st.peek()!='[')
+       {
+           return false;
+       }
+       st.pop();
+       
+   }
+    
+}
+if(st.size()!=0)
+{
+    return false;
+}
+return true;
+    }
+
+}

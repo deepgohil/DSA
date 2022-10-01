415473c47bbdc2e9f6e713fe5ae7d1716b265402
deepgohil <84922741+deepgohil@users.noreply.github.com>
2022-10-01 09:25:02 +0000

    prefix

--- Stack.java
+++ Stack.java
@@ -1,46 +1,48 @@
-import java.io.*;
-import java.util.*;
 
-public class Main {
 
-public static void main(String[] args) throws Exception {
+// import java.io.*;
+// import java.util.*;
 
-Scanner sc=new Scanner(System.in);
-String exp=sc.nextLine();
+// public class Main {
 
-Stack<Character> st=new Stack<>();
+// public static void main(String[] args) throws Exception {
 
-for(int i=0;i<exp.length();i++)
-{
-    char ch=exp.charAt(i);
-    if(ch ==')')
-    {
-        if(st.peek()=='(')
-        {
-           System.out.println("true");
-            return;
-        }
+// Scanner sc=new Scanner(System.in);
+// String exp=sc.nextLine();
+
+// Stack<Character> st=new Stack<>();
+
+// for(int i=0;i<exp.length();i++)
+// {
+//     char ch=exp.charAt(i);
+//     if(ch ==')')
+//     {
+//         if(st.peek()=='(')
+//         {
+//            System.out.println("true");
+//             return;
+//         }
     
-    while(st.peek()!='(')
-    {
-        st.pop();
-    }
-    st.pop();
+//     while(st.peek()!='(')
+//     {
+//         st.pop();
+//     }
+//     st.pop();
     
-    }
-    else
-    {
-        st.push(ch);
-    }
+//     }
+//     else
+//     {
+//         st.push(ch);
+//     }
     
-}
+// }
 
-  System.out.println("false");
+//   System.out.println("false");
 
 
-    }
+//     }
 
-}
+// }
 
 
 
@@ -259,4 +261,83 @@ for(int i=0;i<exp.length();i++)
 // }
 
 
+// 1. You are given a prefix expression.
+// 2. You are required to evaluate it and print it's value.
+// 3. You are required to convert it to infix and print it.
+// 4. You are required to convert it to postfix and print it.
 
+// Note -> Use brackets in infix expression for indicating precedence. Check sample input output for more details.
+
+// Constraints
+// 1. Expression is a valid prefix expression
+// 2. The only operators used are +, -, *, /
+// 3. All operands are single digit numbers.
+
+// Format
+// Input
+// Input is managed for you
+
+// Output
+// value, a number
+// infix
+// prefix
+
+// Example
+// Sample Input
+
+// -+2/*6483
+
+// Sample Output
+// 2
+// ((2+((6*4)/8))-3)
+// 264*8/+3-
+
+// import java.io.*;
+// import java.util.*;
+
+// public class Main{
+  
+
+// public static void main(String[] args) throws Exception {
+//     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//     String exp = br.readLine();
+
+//     // code
+//     Stack<Integer> eval=new Stack<>();
+//     Stack<String> infi=new Stack<>();
+//     Stack<String> postfi=new Stack<>();
+
+//     for (int i = exp.length()-1; i >= 0; i--) {
+//         char ch=exp.charAt(i);
+//         if(ch=='+' || ch=='-' || ch=='*' ||ch=='/')
+//         {
+//             int val_1=eval.pop(),val_2=eval.pop();
+//             if(ch=='+') eval.push(val_1+val_2);
+//             if(ch=='-') eval.push(val_1-val_2);
+//             if(ch=='*') eval.push(val_1*val_2);
+//             if(ch=='/') eval.push(val_1/val_2);
+
+//             String val1=infi.pop(),val2=infi.pop();
+//             infi.push('('+val1+ch+val2+')');
+//             val1=postfi.pop();
+//             val2=postfi.pop();
+//             postfi.push(val1+val2+ch);
+        
+        
+//         }
+
+//         else
+//         {
+//             eval.push(Integer.parseInt(ch+""));
+//             infi.push(ch+"");
+//             postfi.push(ch+"");
+//         }
+        
+//     }
+//     System.out.println(eval.peek());
+//     System.out.println(infi.peek()+'\n'+postfi.peek());
+    
+
+
+//  }
+// }

e598b1650eaa84b00b64ec5bd6b9dbb775dc2c21
deepgohil <84922741+deepgohil@users.noreply.github.com>
2022-10-11 11:42:33 +0000

    Update HashMap.java

--- HashMap.java
+++ HashMap.java
@@ -166,3 +166,113 @@
 //  }
 
 // }
+
+// Longest Consecutive Sequence Of Elements
+// Hard
+
+// 1. You are given a number n, representing the size of array a.
+// 2. You are given n numbers, representing elements of array a.
+// 3. You are required to print the longest sequence of consecutive elements in the array (ignoring duplicates).
+
+// Note -> In case there are two sequences of equal length (and they are also the longest), then print the one for which the starting point of which occurs first in the array.
+
+// Constraints
+// 1 <= n <= 30
+// 0 <= n1, n2, .. n elements <= 15
+
+// Format
+// Input
+// A number n
+// n1
+// n2
+// .. n number of elements
+
+// Output
+// Elements of longest sequence of consecutive elements of array in separate lines (ignoring duplicates)
+
+// Example
+// Sample Input
+
+// 17
+// 12
+// 5
+// 1
+// 2
+// 10
+// 2
+// 13
+// 7
+// 11
+// 8
+// 9
+// 11
+// 8
+// 9
+// 5
+// 6
+// 11
+
+// Sample Output
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
+// 11
+// 12
+// 13
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) throws Exception {
+    // write your code here
+   Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int arr[] =new int[n];
+    for (int i = 0; i < arr.length; i++) {
+    arr[i]=sc.nextInt();
+    }
+ HashMap<Integer,Boolean> hm=new HashMap<>();
+ for (int i = 0; i < arr.length; i++) {
+     hm.put(arr[i],true);
+ }
+
+for (int my_key : hm.keySet()) {
+    if(hm.containsKey(my_key-1))
+    {
+        hm.put(my_key,false);
+    }
+}
+int starting_point=0;
+int counter=0;
+
+for (int my_key : hm.keySet()) {
+   if(hm.get(my_key))
+   {
+    int sp=my_key;
+    int l_counter=1;
+while(hm.containsKey(sp+l_counter))
+{
+l_counter++;
+}
+if(l_counter>counter)
+   {
+       counter=l_counter;
+       starting_point=sp;
+
+   }
+
+   }
+   
+
+}
+
+for (int i = starting_point; i < starting_point+counter; i++) {
+System.out.println(i);    
+}
+ }
+
+}

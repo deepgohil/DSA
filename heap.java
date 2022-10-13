432f730b0eb172079632f9e4d5203e3b5470d7e7
deepgohil <84922741+deepgohil@users.noreply.github.com>
2022-10-13 14:43:53 +0000

    Q3

--- heap.java
+++ heap.java
@@ -159,3 +159,109 @@ public class Main {
    }
 
 }
+
+Merge K Sorted Lists
+Hard
+
+1. You are given a list of lists, where each list is sorted.
+2. You are required to complete the body of mergeKSortedLists function. The function is expected to merge k sorted lists to create one sorted list.
+
+Constraints
+Space complextiy = O(k)
+Time complexity = nlogk
+where k is the number of lists and n is number of elements across all lists.
+
+Format
+Input
+Input is managed for you
+
+Output
+Output is managed for you
+
+Example
+Sample Input
+
+4
+5
+10 20 30 40 50
+7
+5 7 9 11 19 55 57
+3
+1 2 3
+2
+32 39
+
+Sample Output
+1 2 3 5 7 9 10 11 19 20 30 32 39 40 50 55 57 
+	
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+   public static class Pair implements Comparable<Pair>
+   {int li,vi,val;
+   Pair(int li,int vi,int val)
+   {
+      this.li=li;
+      this.vi=vi;
+      this.val=val;
+
+   }
+   public int compareTo(Pair o)
+   {
+      return this.val-o.val;
+   }
+
+   }
+
+   public static ArrayList<Integer> mergeKSortedLists(ArrayList<ArrayList<Integer>> lists){
+      ArrayList<Integer> rv = new ArrayList<>();
+      PriorityQueue<Pair> pq=new PriorityQueue<>();
+
+      for (int i = 0; i < lists.size(); i++) {
+         pq.add(new Pair(i,0,lists.get(i).get(0)));
+      }
+
+      while(pq.size()>0)
+      {
+         Pair temp=pq.remove();
+         rv.add(temp.val);
+         if(temp.vi+1<lists.get(temp.li).size())
+         {
+            temp.vi+=1;
+            temp.val=lists.get(temp.li).get(temp.vi);
+            pq.add(temp);
+         }
+      }
+      
+      // write your code here
+
+      return rv;
+   }
+
+   public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int k = Integer.parseInt(br.readLine());
+      ArrayList<ArrayList<Integer>> lists = new ArrayList<>();
+      for(int i = 0; i < k; i++){
+         ArrayList<Integer> list = new ArrayList<>();
+
+         int n = Integer.parseInt(br.readLine());
+         String[] elements = br.readLine().split(" ");
+         for(int j = 0; j < n; j++){
+            list.add(Integer.parseInt(elements[j]));
+         }
+
+         lists.add(list);
+      }
+
+      ArrayList<Integer> mlist = mergeKSortedLists(lists);
+      for(int val: mlist){
+         System.out.print(val + " ");
+      }
+      System.out.println();
+   }
+
+}
